5d06ef63d72484a9a01ddb0138a6e3982950704a
Julien Guépin <julien@jolicloud.com>
2014-04-16 13:40:37 +0200

    Fixed express config for views and static assets

---
+++ README.md
@@ -0,0 +1,3 @@
+Code for France website
+
+This is an Express server, serving a single webpage, and handling subscriptions with GitHub signup.
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express'),
     mongoose = require('mongoose');
 
-
 var app = express();
 
+// Views
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.render('index.html');
 });
 
 app.listen(3000);
